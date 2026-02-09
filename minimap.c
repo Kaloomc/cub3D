8e36e438b8050f0348113b24ea0b6ccd85e1af41
Lenny <desbouislenny@gmail.fr>
2026-02-09 22:43:37 +0100

    fov camera

--- minimap.c
+++ minimap.c
@@ -6,15 +6,44 @@
 /*   By: fgarnier <fgarnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 00:54:19 by fgarnier          #+#    #+#             */
-/*   Updated: 2026/02/09 14:14:47 by fgarnier         ###   ########.fr       */
+/*   Updated: 2026/02/09 22:42:56 by ldesboui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static void	draw_fov(t_game *game, int size, int offset)
+{
+	int		i;
+	double	x;
+	double	y;
+
+	i = 0;
+	x = (double)(size / 2 + offset);
+	y = (double)(size / 2 + offset);
+	while (i < 5)
+	{
+		x += cos(game->player_angle - (M_PI / 6));
+		y += sin(game->player_angle - (M_PI / 6));
+		my_mlx_pixel_put(game, (int)x, (int)y, 10);
+		++i;
+	}
+	i = 0;
+	x = (double)(size / 2 + offset);
+	y = (double)(size / 2 + offset);
+	while (i < 5)
+	{
+		x += cos(game->player_angle + (M_PI / 6));
+		y += sin(game->player_angle + (M_PI / 6));
+		my_mlx_pixel_put(game, (int)x, (int)y, 10);
+		++i;
+	}
+}
+
 void	draw_cursor(t_game *game, int offset, int size)
 {
 	my_mlx_pixel_put(game, size / 2 + offset, size / 2 + offset, 0);
+	draw_fov(game, size, offset);
 }
 
 void	draw_minimap(t_game *game)
